92cbad3490a75796470123ab2d2197d8864275db
Bruna Sanches <bcpsanches@gmail.com>
2022-01-15 14:31:00 -0300

    Aula 04 - erro

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,13 +4,32 @@ var velocity= Vector2.ZERO
 var move_speed=480
 var gravity = 1200
 var jump_force=-720
+var is_grounded
+onready var raycasts = $raycasts
 
 func _physics_process(delta:float)-> void:
 	velocity.y += gravity * delta
-	var move_direction=int(Input.is_action_pressed("move_right"))- int(Input.is_action_pressed("move_left"))
 	
-	if Input.is_action_pressed(("jump")):
-		velocity.y=jump_force/2
+	_get_input()
+	
+
+	velocity = move_and_slide(velocity)
+	is_grounded=_check_is_ground()
+		
+func _get_input():		
+	var move_direction=int(Input.is_action_pressed("move_right"))- int(Input.is_action_pressed("move_left"))
 	velocity.x=move_speed * move_direction
 	
-	move_and_slide(velocity)
+	if move_direction !=0:
+		$texture.scale.x=move_direction
+		
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") && is_grounded:
+		velocity.y	= jump_force/2
+		
+		
+func _check_is_ground():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+	return false
